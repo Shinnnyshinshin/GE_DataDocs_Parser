c2ed16e707ec9347bf49d6a4955cb0c6260112ea
William Shin <will@superconductive.com>
2020-06-19 17:32:45 -0700

    before cleaning up

--- GE_DataDocs_Parser/GE_DataDocs_Parser.py
+++ GE_DataDocs_Parser/GE_DataDocs_Parser.py
@@ -2,100 +2,35 @@ import ast
 import os
 from collections import namedtuple
 import re
-from typing import Iterator, Optional, Union, List, Mapping, Tuple, Set
+from typing import Iterator, Optional, Union, List, Mapping, Tuple, Set, Dict
 import logging
 import json
 
-
 logger = logging.getLogger(__name__)
 
-ANNOTATION_KEY = "id:"
 
-MaturityDetails = namedtuple("MaturityDetails", ["api_stability", "implementation_completeness", "unit_test_coverage", "integration_infrastructure_test_coverage", "documentation_completeness", "bug_risk"])
-FeatureAnnotation = namedtuple("FeatureAnnotation", ["id", "title", "icon", "short_description", "description", "how_to_guide_url", "maturity", "maturity_details"])
+ANNOTATION_REGEX = ""
+ANNOTATION_REGEX += "[ ]*(id:.*)[\n]"
+ANNOTATION_REGEX += "[ ]*(title:.*)[\n]"
+ANNOTATION_REGEX += "[ ]*(icon:.*)[\n]"
+ANNOTATION_REGEX += "[ ]*(short_description:.*)[\n]"
+ANNOTATION_REGEX += "[ ]*(description:.*)[\n]"
+ANNOTATION_REGEX += "[ ]*(how_to_guide_url:.*)[\n]"
+ANNOTATION_REGEX += "[ ]*(maturity:.*)[\n]"
+ANNOTATION_REGEX += "[ ]*(maturity_details:.*)[\n]"
+ANNOTATION_REGEX += "[ ]*(api_stability:.*)[\n]"
+ANNOTATION_REGEX += "[ ]*(implementation_completeness:.*)[\n]"
+ANNOTATION_REGEX += "[ ]*(unit_test_coverage:.*)[\n]"
+ANNOTATION_REGEX += "[ ]*(integration_infrastructure_test_coverage:.*)[\n]"
+ANNOTATION_REGEX += "[ ]*(documentation_completeness:.*)[\n]"
+ANNOTATION_REGEX += "[ ]*(bug_risk:.*)[\n]"
+annotation_regex_compiled = re.compile(ANNOTATION_REGEX)
 
+maturity_details_keys = ["api_stability", "implementation_completeness", "unit_test_coverage", "integration_infrastructure_test_coverage", "documentation_completeness", "bug_risk"]
 
-FULL_REGEX_MATCH = ""
-FULL_REGEX_MATCH += "[ ]*(id:.*)[\n]" # 1
-FULL_REGEX_MATCH += "[ ]*(title:.*)[\n]" # 2
-FULL_REGEX_MATCH += "[ ]*(icon:.*)[\n]" # 3
-FULL_REGEX_MATCH += "[ ]*(short_description:.*)[\n]" # 4
-FULL_REGEX_MATCH += "[ ]*(description:.*)[\n]"# 5
-FULL_REGEX_MATCH += "[ ]*(how_to_guide_url:.*)[\n]"# 6
-FULL_REGEX_MATCH += "[ ]*(maturity:.*)[\n]"# 7
-FULL_REGEX_MATCH += "[ ]*(maturity_details:.*)[\n]"# 8
-FULL_REGEX_MATCH += "[ ]*(api_stability:.*)[\n]" #9
-FULL_REGEX_MATCH += "[ ]*(implementation_completeness:.*)[\n]" #10
-FULL_REGEX_MATCH += "[ ]*(unit_test_coverage:.*)[\n]" #11
-FULL_REGEX_MATCH += "[ ]*(integration_infrastructure_test_coverage:.*)[\n]" #12
-FULL_REGEX_MATCH += "[ ]*(documentation_completeness:.*)[\n]" #13
-FULL_REGEX_MATCH += "[ ]*(bug_risk:.*)[\n]" #14
-
-
-
-ANNOTATION_REGEX_ONLY = ''
-
-
-
-maturitydetails_keys = ["api_stability", "implementation_completeness", "unit_test_coverage", "integration_infrastructure_test_coverage", "documentation_completeness", "bug_risk"]
-
-
-
-pattern = re.compile(FULL_REGEX_MATCH)
-
-
-"""
-    id: expectations_store_git
-    title: Expectation Store - Git
-    icon:
-    short_description:
-    description:
-    how_to_guide_url:
-    maturity: Production
-    maturity_details:
-        api_stability: Stable
-        implementation_completeness: Complete
-        unit_test_coverage: Complete
-        integration_infrastructure_test_coverage: N/A
-        documentation_completeness: Complete
-        bug_risk: Low
-
-
-
-
-
-{"id": "expectations_store_s3"
-    title: Expectation Store - S3
-    icon:
-    short_description:
-    description:
-    how_to_guide_url:
-    maturity: Beta
-    maturity_details":{
-        api_stability: Stable
-        implementation_completeness: Complete
-        unit_test_coverage: Complete
-        integration_infrastructure_test_coverage: Minimal
-        documentation_completeness: Complete
-        bug_risk: Low}
-}
-
-# list of dicts [{s3 *****}, {azureblob *****}, {gcs *****}]
-
-if icon is null :
-    https://great-expectations-web-assets.s3.us-east-2.amazonaws.com/feature_maturity_icons/{id}.png"
-if not then keep
-
-
-"""
-
-
-AnnotatedNode = namedtuple("AnnotatedNode", ["name", "path", "annotation", "type_", "id"])
-#AnnotatedEdge = namedtuple("AnnotatedEdge", ["id_1", "id_2", "annotation"])
-#EdgeAnnotation = namedtuple("EdgeAnnotation", ["edge_type"])
+AnnotatedNode = namedtuple("AnnotatedNode", ["name", "path", "annotation", "type_"])
 
 def build_annotations(path: str) -> Mapping[str, Mapping]:
-
     annotation_tree = {
         "store_backend": {
             "title": "Backend stores",
@@ -103,10 +38,6 @@ def build_annotations(path: str) -> Mapping[str, Mapping]:
                            "data docs, and other Data Context artifacts."
         }
     }
-
-
-
-
 
     nodes = []
     logger.info(f"working through path {path}")
@@ -116,23 +47,10 @@ def build_annotations(path: str) -> Mapping[str, Mapping]:
     for filepath, ast_tree in directory_iter:
         nodes = nodes + walk_tree(filepath, ast_tree)
 
-    nodes = [node for node in nodes if node.annotation is not None]
-    #print(nodes)
+    for node in nodes:
+        print(node)
     feature_types = ""
-    #feature_types = set([node.annotation.id for node in nodes])
-    #for feature_type in feature_types:
-    #   if feature_type not in annotation_tree:
-    #        annotation_tree[feature_type] = dict()
-    #    annotation_tree[feature_type]["cases"] = [
-    #        {
-    #            "Feature": node.annotation.Feature,
-    #            "Summary": node.annotation.Summary,
-    #            "API_Stability": node.annotation.API_Stability,
-    #        }
-    #        for node in nodes if node.annotation.feature_type == feature_type
-    #    ]
     return feature_types
-
 
 
 def walk_directory(path: str) -> Iterator[ast.AST]:
@@ -149,16 +67,14 @@ def walk_directory(path: str) -> Iterator[ast.AST]:
                 yield os.path.relpath(filepath, path), ast.parse(srcfile.read())
 
 
-
-
-def walk_tree(basename: str, tree: ast.AST, include_empty: bool = True) -> List[AnnotatedNode]:
+def walk_tree(basename: str, tree: ast.AST, include_empty: bool = False) -> List[AnnotatedNode]:
     """Parse AST tree, generating nodes and edges."""
     nodes = []
+    annotation = []
     for node in ast.walk(tree):
         if not isinstance(node, (ast.Module, ast.ClassDef, ast.FunctionDef)):
             continue
         annotation = _parse_feature_annotation_regex(ast.get_docstring(node))
-        print(annotation)
 
         if isinstance(node, ast.Module):
             name = os.path.basename(basename)
@@ -166,16 +82,16 @@ def walk_tree(basename: str, tree: ast.AST, include_empty: bool = True) -> List[
             name = node.name
         if name.endswith(".py"):
             name = name[:-3]
-        if annotation is not None or include_empty:
-            nodes.append(AnnotatedNode(name, basename, annotation, type(node).__name__, name))
+
+        if annotation is not None and len(annotation) > 0 or include_empty:
+            nodes.append(AnnotatedNode(name, basename, annotation, type(node).__name__))
+
     return nodes
 
 
-
-
-def _parse_feature_annotation_regex(docstring: Union[str, List[str], None]) -> Optional[FeatureAnnotation]:
+def _parse_feature_annotation_regex(docstring: Union[str, List[str], None]) -> Dict[str:str]:
     """Parse a docstring and return a feature annotation."""
-    list_of_annotation = []
+    list_of_annotations = []
     id_val = ""
     if docstring is None:
         return
@@ -183,10 +99,7 @@ def _parse_feature_annotation_regex(docstring: Union[str, List[str], None]) -> O
         for matches in re.findall(pattern, docstring):
             annotation_dict = dict() # create new dictionary for each match
             maturity_details_dict = dict()
-            #for key in FeatureAnnotation._fields:
-            # reversed so that we bduilg the MAturityDetails first
             for matched_line in matches:
-                #print(matched_line)
                 # split matched line_fields
                 matched_line_fields = matched_line.split(":")
                 this_key = matched_line_fields[0].strip()
@@ -195,7 +108,6 @@ def _parse_feature_annotation_regex(docstring: Union[str, List[str], None]) -> O
                 if this_key == "id":
                     id_val = this_val
 
-                #temp_maturity = MaturityDetails()
                 if this_key in maturitydetails_keys:
                     maturity_details_dict[this_key] = this_val
                 elif this_key == "icon": # icon is a special cases
@@ -209,49 +121,11 @@ def _parse_feature_annotation_regex(docstring: Union[str, List[str], None]) -> O
             annotation_dict["maturity_details"] = maturity_details_dict
             if annotation_dict is not None:
                 list_of_annotation.append(annotation_dict)
-    return(list_of_annotation)
-
-
-
-def _parse_feature_annotation_ast(docstring: Union[str, List[str], None]) -> Optional[FeatureAnnotation]:
-    """Parse a docstring and return a feature annotation."""
-    if docstring is None:
-        return
-    if isinstance(docstring, str):
-        docstring = docstring.splitlines()
-    in_annotation = False
-    annotation = dict()
-    for line in docstring:
-        line = line.lstrip()
-        if in_annotation and line == "":
-
-            in_annotation = False
-            if len(annotation) > 0:
-                yield FeatureAnnotation(**annotation)
-
-        elif line[0:3] == ANNOTATION_KEY:
-            logger.debug("Found annotation")
-            in_annotation = True
-
-        if in_annotation:
-            line_fields = line.split(":", 1)
-            for key in FeatureAnnotation._fields:
-                if line_fields[0] == key:
-                    try:
-                        annotation[key] = line_fields[1].strip()
-                    except IndexError:
-                        logger.warning(f"Found annotation with key {key}, but no value.")
-                elif key == "Summary":
-                    if line_fields[0].replace(" ", "")[0:2] == "->":
-                        try:
-                            annotation["Summary"] = line_fields[0].replace(" ", "").split('->')[1]
-                        except IndexError:
-                            logger.warning(f"Found annotation with key {key}, but no value.")
-
+    return(list_of_annotations)
 
 
 def main():
-    res = build_annotations("/Users/work/Development/GE_DataDocs_Parser/test_folder")
+    res = build_annotations("/Users/work/Development/great_expectations")
 
 if __name__ == "__main__":
     logging.basicConfig(filename='will.log', level=logging.WARNING)
