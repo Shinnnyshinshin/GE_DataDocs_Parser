77f3bc5cad34f3a47da5506013631888988bf5e4
William Shin <will@superconductive.com>
2020-06-24 16:50:55 -0700

    added documentation

--- GE_DataDocs_Parser/GE_DataDocs_Parser.py
+++ GE_DataDocs_Parser/GE_DataDocs_Parser.py
@@ -3,10 +3,11 @@ import json
 import logging
 import os
 import re
-from typing import Iterator, Union, List, Dict
+from typing import Iterator, Union, List, Dict, Type
 
 logger = logging.getLogger(__name__)
 
+# REGEX that is used to extract annotation information from DataDocs
 ANNOTATION_REGEX = ""
 ANNOTATION_REGEX += "[ ]*(id:.*)[\n]"
 ANNOTATION_REGEX += "[ ]*(title:.*)[\n]"
@@ -24,43 +25,74 @@ ANNOTATION_REGEX += "[ ]*(documentation_completeness:.*)[\n]"
 ANNOTATION_REGEX += "[ ]*(bug_risk:.*)[\n]"
 annotation_regex_compiled = re.compile(ANNOTATION_REGEX)
 
+# for extracting nested dict that contains maturity details
 maturity_details_keys = ["api_stability", "implementation_completeness", "unit_test_coverage", "integration_infrastructure_test_coverage", "documentation_completeness", "bug_risk"]
 
-# global dictionary for all annotations that already exist in DataDocs
+# all annotation fields are stored as a global dict
 full_annotation = Dict[str, str]
 
 
-def build_annotations(folder_to_annotate: str, in_json: str):
-    """ Entry-point into parser script. Will return the results of process_toc, whichi """
-    build_full_annotation_dict(folder_to_annotate)
-    return process_toc(in_json)
+def build_annotations(folder_to_annotate: object, in_json: object) -> object:
+    """
+    Entry-point into parser script from CLI
+
+    Args:
+        folder_to_annotate: PATH to Great Expectations folder
+        in_json: TOC file for all Feature Maturity Grid annotations
+
+    Returns:
+        JSON that can be used as input for the Feature Maturity Grid
+    """
+    _build_full_annotation_dict(folder_to_annotate)
+    return _process_toc(in_json)
 
 
+def _build_full_annotation_dict(path: str) -> None:
+    """
+    Args:
+        path: PATH to Great Expectations folder (passed in from build_annotations function)
 
-def process_toc(in_json: str):
+    Returns:
+        None: will modify global full_annotation dict directly
+
+    """
+    logger.info(f"working through path {path}")
+    path = os.path.abspath(path)
+    directory_iter = _walk_directory(path)  # iterator(filepath of python file, ast object)
+    for filepath, ast_tree in directory_iter:
+        _walk_tree(filepath, ast_tree)
+
+
+def _process_toc(in_json: str) -> Dict[str, str]:
+    """
+        Updates the TOC JSON file to include the annotations we have parsed from DataDocs
+    Args:
+        in_json: TOC file for all Feature Maturity Grid annotations (passed in from build_annotations)
+
+    Returns:
+        dictionary that is the updated TOC JSON
+    """
     with open(in_json) as json_file:
-        data = json.load(json_file)
-        for title in data:
+        loaded_json = json.load(json_file)
+        for title in loaded_json:
             for section_features in title["section_features"]:
                 all_cases = section_features["cases"]
                 for index in range(len(all_cases)):
                     if all_cases[index]["id"] in full_annotation.keys():
                         all_cases[index] = full_annotation[all_cases[index]["id"]]
                 section_features["cases"] = all_cases
-    return data
+    return loaded_json
 
 
-def build_full_annotation_dict(path: str) -> Dict[str, str]:
-    logger.info(f"working through path {path}")
-    path = os.path.abspath(path)
-    directory_iter = walk_directory(path) # iterator(filepath of python file, ast object)
-    for filepath, ast_tree in directory_iter:
-        walk_tree(filepath, ast_tree)
-    return full_annotation
+def _walk_directory(path: str) -> Iterator[ast.AST]:
+    """
+    Does an os.walk through the input PATH to read all .py files and load them as abstract syntax tree (AST) objects, which can be parsed
+    Args:
+        path: path of great_expectations folder
 
-
-def walk_directory(path: str) -> Iterator[ast.AST]:
-    """Walk a directory and provide AST trees for each item."""
+    Returns:
+        Iterator of resulting AST objects
+    """
     logger.info(f"Beginning to parse path {path}")
     for root, dirs, files in os.walk(path):
         for file in files:
@@ -73,28 +105,45 @@ def walk_directory(path: str) -> Iterator[ast.AST]:
                 yield os.path.relpath(filepath, path), ast.parse(srcfile.read())
 
 
-def walk_tree(basename: str, tree: ast.AST, include_empty: bool = False):
-    """Parse AST tree, generating dictionary of annotations"""
-    annotation = []
+def _walk_tree(tree: ast.AST) -> None:
+    """
+
+    Args:
+        tree: ast.AST tree for each .py file in Great Expectations directory
+
+    Returns:
+        None: modifies global full_annotation dict directly.
+    """
+    annotation_list = None
     for node in ast.walk(tree):
         if not isinstance(node, (ast.Module, ast.ClassDef, ast.FunctionDef)):
             continue
-        annotation = _parse_feature_annotation(ast.get_docstring(node))
-        if annotation:
-            for annot in annotation:
-                full_annotation[annot["id"]] = annot
+        annotation_list = _parse_feature_annotation(ast.get_docstring(node))
+        if annotation_list is not None:
+            for annotation in annotation_list:
+                # key is annotation["id"] and
+                full_annotation[annotation["id"]] = annotation
 
 
-def _parse_feature_annotation(docstring: Union[str, List[str], None]):
-    """Parse a docstring and return a feature annotation."""
+
+def _parse_feature_annotation(docstring: Union[str, List[str], None]) -> List[Type[Dict[any, any]]]:
+    """
+
+    Args:
+        docstring: docstring object that is parsed from ast.get_docstring(node) (passed in from _walk_tree method)
+
+    Returns:
+        list_of_annotations: list of dictionaries that contain one annotation per dictionay
+    """
+
     list_of_annotations = []
     id_val = ""
     if docstring is None:
         return
     if isinstance(docstring, str):
         for matches in re.findall(annotation_regex_compiled, docstring):
-            annotation_dict = dict() # create new dictionary for each match
-            maturity_details_dict = dict()
+            annotation_dict = Dict[str, str] # create new dictionary for each match
+            maturity_details_dict = Dict[str, str]
             for matched_line in matches:
                 # split matched line_fields
                 matched_line_fields = matched_line.split(":")
@@ -117,4 +166,5 @@ def _parse_feature_annotation(docstring: Union[str, List[str], None]):
             annotation_dict["maturity_details"] = maturity_details_dict
             if annotation_dict is not None:
                 list_of_annotations.append(annotation_dict)
-    return(list_of_annotations)
+
+    return list_of_annotations
